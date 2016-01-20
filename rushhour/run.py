fa77e89e2dac8e87edeef85a5a12db3b056f1edb
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2016-01-20 14:47:11 +0100

    Add command line module to parse and solve a board.

---
+++ rushhour/run.py
@@ -0,0 +1,35 @@
+import sys
+
+from rushhour.board import board_from_string, Direction
+from rushhour.solver import solve
+
+
+def _sign(v):
+    return v / abs(v)
+
+
+if __name__ == "__main__":
+    # Read the board
+    if len(sys.argv) == 1:
+        board_description = sys.stdin.read()
+    else:
+        with open(sys.argv[1]) as fp:
+            board_description = fp.read()
+
+    # Solve the board
+    board = board_from_string(board_description)
+    moves = solve(board)
+
+    # Render the board
+    for move in moves:
+        direction_string = {
+            (Direction.horizontal, 1): 'R',
+            (Direction.horizontal, -1): 'L',
+            (Direction.vertical, 1): 'D',
+            (Direction.vertical, -1): 'U',
+        }[move[1], _sign(move[2])]
+        print("{}{}{}".format(
+            move[0].color,
+            direction_string,
+            abs(move[2]),
+        ))
